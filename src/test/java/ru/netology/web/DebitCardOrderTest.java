628d99090ab7bd47f6dd39faadf0d889986137d1
Pavel Pyanykh <qualityassurance@fastmail.com>
2022-05-20 15:12:38 +0300

    modified request data

--- src/test/java/ru/netology/web/DebitCardOrderTest.java
+++ src/test/java/ru/netology/web/DebitCardOrderTest.java
@@ -45,7 +45,7 @@ public class DebitCardOrderTest {
         driver.findElement(By.tagName("button")).click();
 
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        assertEquals("Ваша заявка успешно отправлена. Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     @Test
