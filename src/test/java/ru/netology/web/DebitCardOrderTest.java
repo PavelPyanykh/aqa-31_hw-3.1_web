0a24e04d7556c1fcab2cf331b366cc8a201c87cf
Pavel Pyanykh <qualityassurance@fastmail.com>
2022-05-21 17:53:56 +0300

    modified test class

--- src/test/java/ru/netology/web/DebitCardOrderTest.java
+++ src/test/java/ru/netology/web/DebitCardOrderTest.java
@@ -28,6 +28,7 @@ public class DebitCardOrderTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -38,7 +39,6 @@ public class DebitCardOrderTest {
 
     @Test
     public void shouldSubmitApplication() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Александр");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79997772233");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -49,8 +49,7 @@ public class DebitCardOrderTest {
     }
 
     @Test
-    public void shouldNotSubmitApplicationEmptyField() {
-        driver.get("http://localhost:9999");
+    public void shouldNotSubmitEmptyName() {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79997772233");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -61,8 +60,7 @@ public class DebitCardOrderTest {
     }
 
     @Test
-    public void shouldNotSubmitApplicationLatinField() {
-        driver.get("http://localhost:9999");
+    public void shouldNotSubmitLatinName() {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivanov Aleksandr");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79997772233");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -70,6 +68,39 @@ public class DebitCardOrderTest {
 
         String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    public void shouldNotSubmitEmptyPhone() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Александр");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    public void shouldNotSubmitInvalidPhone() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Александр");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7999777");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    public void shouldNotSubmitUnchecked() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Александр");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79997772233");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]"));
+        driver.findElement(By.tagName("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid .checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
     }
 
 }
